614ca2cc0c61e5fdb2b2fb2cc813f00872b78c69
maestermatt <mecha04@me-192-118-08>
2018-02-08 16:05:09 -0800

    adding comments

--- main.py
+++ main.py
@@ -25,27 +25,30 @@ data = Queue('I', 800)
 adc = None
 
 def interrupt(timer):
+    '''
+    This is the ISR that is run when the timer runs out
+    It won't overflow data. Instead it will stop putting values in the Queue
+    '''
     global data, adc
     if adc is not None and not data.full():
         data.put(adc.read())
 
 if __name__ == "__main__":
-
-    pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.IN)
-    pinC1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP)
+    pinC0 = pyb.Pin(pyb.Pin.board.PC0, pyb.Pin.IN) # Setting pin A5 as input to the ADC
+    pinC1 = pyb.Pin(pyb.Pin.board.PC1, pyb.Pin.OUT_PP) # Setting pin A4 as output 
     adc = pyb.ADC(pinC0)
 
     pinC1.low()
-    tim1 = pyb.Timer(1, freq=1000, callback=interrupt)
+    tim1 = pyb.Timer(1, freq=1000, callback=interrupt) #Setting Timer to 1kHz
     pinC1.high()
 
-    while True:
+    while True: # Run until data is full
         if data.full():
             break
 
-    tim1.callback(None)
+    tim1.callback(None) # prevent calling back
 
     while not data.empty():
-        print(data.get()*0.000805)
+        print(data.get()*0.000805) #Convert values to voltage
     pinC1.low()
 
